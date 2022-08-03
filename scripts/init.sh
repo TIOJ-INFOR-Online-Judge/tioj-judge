d5b1e5b57acf05f55bdb9ed17cf06b115ae29fd3
adrien1018 <adrien1018@users.noreply.github.com>
2022-08-03 19:45:30 +0800

    Fix cgroups v2 in docker

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,6 +8,15 @@ JUDGE_MAX_RSS_PER_TASK_MB="${JUDGE_MAX_RSS_PER_TASK_MB:-2048}"
 JUDGE_MAX_OUTPUT_PER_TASK_MB="${JUDGE_MAX_OUTPUT_PER_TASK_MB:-1024}"
 JUDGE_MAX_SUBMISSION_QUEUE_SIZE="${JUDGE_MAX_SUBMISSION_QUEUE_SIZE:-500}"
 
+# cgroup2: add memory to subtree_control
+if mount | grep -Pq 'cgroup2? on \K([^ \t]+)(?= type cgroup2)'; then
+  mkdir /sys/fs/cgroup/tmp_child
+  for i in $(cat /sys/fs/cgroup/cgroup.procs); do
+    echo $i > /sys/fs/cgroup/tmp_child/cgroup.procs
+  done
+  echo '+memory' > /sys/fs/cgroup/cgroup.subtree_control
+fi
+
 cat > /etc/tioj-judge.conf <<EOF
 tioj_url = $TIOJ_URL
 tioj_key = $TIOJ_KEY
