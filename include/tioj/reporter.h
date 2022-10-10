737c392cc2703445ab087c7420563092e5037991
adrien1018 <adrien1018@users.noreply.github.com>
2022-10-10 11:14:00 +0000

    Add default implementation to Reporter

--- include/tioj/reporter.h
+++ include/tioj/reporter.h
@@ -6,12 +6,12 @@ class Submission;
 class Reporter {
  public:
   // these functions should not block
-  virtual void ReportStartCompiling(const Submission&) = 0;
-  virtual void ReportOverallResult(const Submission&) = 0;
-  virtual void ReportScoringResult(const Submission&, int subtask) = 0;
-  virtual void ReportCEMessage(const Submission&) = 0;
-  virtual void ReportERMessage(const Submission&) = 0;
-  virtual void ReportFinalized(const Submission&, size_t queue_size_before_pop) = 0;
+  virtual void ReportStartCompiling(const Submission&) {}
+  virtual void ReportOverallResult(const Submission&) {}
+  virtual void ReportScoringResult(const Submission&, int subtask) {}
+  virtual void ReportCEMessage(const Submission&) {}
+  virtual void ReportERMessage(const Submission&) {}
+  virtual void ReportFinalized(const Submission&, size_t queue_size_before_pop) {}
 };
 
 #endif  // TIOJ_REPORTER_H_
